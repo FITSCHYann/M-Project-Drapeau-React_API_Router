5c698d54d2cfaecb93b5a3655cfb12c7aeac2b8a
FITSCH YANN <fitsch-yann@hotmail.fr>
2022-10-23 19:01:23 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate sequi unde aut autem consequatur praesentium rem voluptas assumenda, dolorum harum.
+            </p>
+        </div>
+    );
+};
+
+export default About;
